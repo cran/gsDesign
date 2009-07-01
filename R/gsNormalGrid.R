a1f2c1503b1fe157671bc3bb5719fa31f42c5db5
Keaven Anderson <keaven_anderson@merck.com>
2009-07-01 12:03:13 +0000

    version 2.0-5

---
+++ R/gsNormalGrid.R
@@ -0,0 +1,59 @@
+##################################################################################
+#  Normal density grid functionality for the gsDesign package
+#
+#  Exported Functions:
+#                   
+#    normalGrid
+#
+#  Hidden Functions:
+#
+#    (none)
+#
+#  Author(s): Keaven Anderson, PhD.
+# 
+#  Reviewer(s): REvolution Computing 19DEC2008 v.2.0 - William Constantine, Kellie Wills 
+#
+#  R Version: 2.7.2
+#
+##################################################################################
+
+###
+# Exported Functions
+###
+
+"normalGrid" <- function(r=18, bounds=c(0, 0), mu=0, sigma=1)
+{    
+    checkScalar(r,"integer", c(1, 80))
+    checkVector(bounds,"numeric")
+    checkScalar(mu, "numeric")
+    checkScalar(sigma, "numeric", c(0, Inf), c(FALSE, TRUE))
+    
+    if (length(bounds) != 2)
+    {
+        stop("bounds variable in normalGrid must be numeric and have length 2")
+    }
+    
+    # produce grid points and weights for numerical integration of normal density    
+    storage.mode(r) <- "integer"
+    
+    z <- as.double(c(1:(12 * r - 3)))
+    w <- z
+
+    if (bounds[1] == 0. && bounds[2] == 0.)
+    {    
+        bounds[1] <- mu - 6 * sigma
+        bounds[2] <- mu + 6 * sigma
+    }
+    else if (bounds[2] <= bounds[1])
+    {
+        return(list(z=NULL, wgts=NULL))        
+    }
+
+    b <- as.double((bounds-mu) / sigma)
+    xx <- .C("stdnorpts", r, b, z, w)
+    len <- sum(xx[[3]] <= b[2])
+    z <- xx[[3]][1:len] * sigma + mu
+    w <- xx[[4]][1:len] * sigma * dnorm(z, mean=mu, sd=sigma)
+    
+    list(z=z, wgts=w)
+}
